ff5bccb6be420fb3ada9730b6616d5e720455f00
Idontknowhowtocode420 <w0488576@nscc.ca>
2023-10-23 10:25:22 -0300

    pong

--- pong.py
+++ pong.py
@@ -1,13 +1,35 @@
 import turtle
 
 wn = turtle.Screen()
+wn.title("Pong")
 wn.bgcolor("black")
+wn.setup(width=800, height=600)
 wn.tracer(0)
 
 paddle_a = turtle.Turtle()
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.speed(0)
-paddle_a.shapesize(stretch_vid=5,stretch_len=1)
+paddle_a.speed(10)
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350,0)
+
+paddle_b = turtle.Turtle()
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.speed(10)
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+balls = turtle.Turtle()
+balls.speed(0)
+balls.shape("circle")
+balls.color("orange")
+balls.penup()
+balls.goto(0,0)
+
+
+
+while True:
+    wn.update()
