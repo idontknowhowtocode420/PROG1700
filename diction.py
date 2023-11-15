c358fedcf27475ea67177c5280665d5be50a6646
Idontknowhowtocode420 <w0488576@nscc.ca>
2023-11-15 14:19:07 -0400

    chnge

--- diction.py
+++ diction.py
@@ -4,8 +4,8 @@ student_scores = {}
 
 student_scores = {
     "davis": 90,
-    "aiden": 85,
-    "tegan": 78,
+    "tegan": 85,
+    "aiden": 78,
     "ethan": 92,
     "zack": 88
 }
@@ -18,20 +18,20 @@ student_name = input("enter a students name to check their score ")
 if student_name in student_scores:
     print(f"{student_name}s score is {student_scores[student_name]}")
 else:
-    print(f"student {student_name} not found in the dictonary")
+    print(f"student {student_name} not found in the records.")
 update_student = input("enter the name of the student to update their score ")
 if update_student in student_scores:
     new_score = int(input(f"enter {update_student}s new score: "))
     student_scores[update_student] = new_score
     print(f"{update_student}s score has been updated to {new_score}")
 else:
-    print(f"Student {update_student} not found in the dictionary")
+    print(f"student {update_student} not found in the record")
 remove_student = input("enter the name of the student to remove ")
 if remove_student in student_scores:
     del student_scores[remove_student]
-    print(f"{remove_student} has been removed from the disctonary")
+    print(f"{remove_student} has been removed from the record")
 else:
-    print(f"student {remove_student} not found in the dictionary")
+    print(f"student {remove_student} not found in the record")
 highscore = max(student_scores.values())
-highscore = [student for student, score in student_scores.items() if score == highscore][0]
-print(f"highest grade: {highscore} was done by {highscore}")
+highestscorer = [student for student, score in student_scores.items() if score == highscore][0]
+print(f"Highest Score: {highscore} achieved by {highestscorer}")
